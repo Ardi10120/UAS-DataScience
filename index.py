125485849c9a1091ba772b90a61d37b8888f7d73
Ardi10120 <91103615+Ardi10120@users.noreply.github.com>
2024-07-10 13:28:05 +0700

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ import pandas as pd
 import itertools
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
@@ -105,53 +104,7 @@ y = selected_features['num']  # Kolom target 'num'
 # Split data menjadi data latih dan data uji
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Inisialisasi model Decision Tree
-dt_model = DecisionTreeClassifier(random_state=42)
-dt_model.fit(X_train, y_train)
-y_pred_dt = dt_model.predict(X_test)
 
-# Evaluasi model Decision Tree
-acc_dt = accuracy_score(y_test, y_pred_dt)
-print("Akurasi Decision Tree:", acc_dt)
-if acc_dt >= 0.7:
-    print("Model Decision Tree memenuhi syarat minimal akurasi 70%")
-else:
-    print("Model Decision Tree tidak memenuhi syarat minimal akurasi 70%")
-print("Confusion Matrix Decision Tree:")
-cm_dt = confusion_matrix(y_test, y_pred_dt)
-print(cm_dt)
-
-# Plot confusion matrix Decision Tree
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm_dt, annot=True, cmap='Blues', fmt='d', xticklabels=dt_model.classes_, yticklabels=dt_model.classes_)
-plt.title('Confusion Matrix - Decision Tree')
-plt.xlabel('Predicted')
-plt.ylabel('Actual')
-plt.show()
-
-# Inisialisasi model Random Forest
-rf_model = RandomForestClassifier(random_state=42)
-rf_model.fit(X_train, y_train)
-y_pred_rf = rf_model.predict(X_test)
-
-# Evaluasi model Random Forest
-acc_rf = accuracy_score(y_test, y_pred_rf)
-print("\nAkurasi Random Forest:", acc_rf)
-if acc_rf >= 0.7:
-    print("Model Random Forest memenuhi syarat minimal akurasi 70%")
-else:
-    print("Model Random Forest tidak memenuhi syarat minimal akurasi 70%")
-print("Confusion Matrix Random Forest:")
-cm_rf = confusion_matrix(y_test, y_pred_rf)
-print(cm_rf)
-
-# Plot confusion matrix Random Forest
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm_rf, annot=True, cmap='Greens', fmt='d', xticklabels=rf_model.classes_, yticklabels=rf_model.classes_)
-plt.title('Confusion Matrix - Random Forest')
-plt.xlabel('Predicted')
-plt.ylabel('Actual')
-plt.show()
 
 # Inisialisasi model SVM
 svm_model = SVC(random_state=42)
